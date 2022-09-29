f3264c411fcbb2c2bcd628dcfbc284a33222fa08
Ilya Novik <ilyanovik@Ilyas-MacBook-Pro.local>
2022-09-29 19:07:27 +0200

    refactor: fix section and userinfo instances

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,12 +6,12 @@ export default class Section {
   }
 
   addItem(element) {
-    this._container.appednd(element);
+    this._container.prepend(element);
   }
 
   renderItems() {
-    this._renderedItems.forEach((item) => {
-      this._renderer(item);
+    this._renderedItems.forEach((cardData) => {
+      this._renderer(cardData);
     })
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,23 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import { selectors } from "../utils/constants.js";
-import './index.css';
+import "./index.css";
+import UserInfo from "../components/UserInfo.js";
+import Section from "../components/Section.js";
 
-const cardElements = document.querySelector(selectors.cardElements);
+// const cardElements = document.querySelector(selectors.cardElements);
+const sectionInstance = new Section(
+  {
+    items: initialCards,
+    renderer: (cardData) => {
+      const card = createCard(cardData);
+      sectionInstance.addItem(card);
+    },
+  },
+  selectors.cardElements
+);
 
-initialCards.forEach((cardData) => {
-  const newCard = createCard(cardData);
-  renderItem(cardElements, newCard);
-});
-
-function renderItem(wrap, card) {
-  wrap.prepend(card);
-}
+sectionInstance.renderItems();
 
 function createCard(cardData) {
   const newCard = new Card(cardData, selectors.cardTemplate, handleCardClick);
@@ -23,7 +28,7 @@ function createCard(cardData) {
   return cardElement;
 }
 
-function handleCardClick (name, link) {
+function handleCardClick(name, link) {
   const popupWithImage = new PopupWithImage(selectors.popupWithImage);
   popupWithImage.open(link, name);
 }
@@ -36,11 +41,11 @@ const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
     const validator = new FormValidator(config, formElement);
-    const formName = formElement.getAttribute('name');
+    const formName = formElement.getAttribute("name");
     formValidators[formName] = validator;
     formValidators[formName].enableValidation();
-  })
-}
+  });
+};
 
 enableValidation({
   formSelector: selectors.formSelector,
@@ -50,7 +55,7 @@ enableValidation({
   inputErrorClass: selectors.inputErrorClass,
   inputErrorClassBase: selectors.inputErrorClassBase,
   errorClassVisible: selectors.errorClassVisible,
-})
+});
 
 // add a new card
 
@@ -61,47 +66,52 @@ const popupPlaceLink = document.querySelector(selectors.popupPlaceLink);
 const popupWithPlaceForm = new PopupWithForm(selectors.formPlaceSelector, formPlaceSubmitHandler);
 
 addCardButton.addEventListener("click", () => {
-  popupPlaceName.value = '';
-  popupPlaceLink.value = '';
+  popupPlaceName.value = "";
+  popupPlaceLink.value = "";
   formValidators.placeForm.resetValidation();
   formValidators.placeForm.toggleButtonState();
   popupWithPlaceForm.open();
 });
 
 function formPlaceSubmitHandler(evt) {
-    evt.preventDefault();
-    const newCardData = {};
-    newCardData.name = popupPlaceName.value;
-    newCardData.link = popupPlaceLink.value;
-  
-    const newCard = createCard(newCardData, selectors.cardTemplate, handleCardClick)
-    renderItem(cardElements, newCard);
-    popupWithPlaceForm.close();
+  evt.preventDefault();
+  const newCardData = {};
+  newCardData.name = popupPlaceName.value;
+  newCardData.link = popupPlaceLink.value;
+  const newCard = createCard(newCardData, selectors.cardTemplate, handleCardClick);
+  sectionInstance.addItem(newCard)
+  popupWithPlaceForm.close();
 }
 
 // edit profile
 const profileEditButton = document.querySelector(selectors.profileEditButton);
 const nameInput = document.querySelector(selectors.nameInput);
 const jobInput = document.querySelector(selectors.jobInput);
-const profileNameElement = document.querySelector(selectors.profileNameElement);
-const profileJobElement = document.querySelector(selectors.profileJobElement);
 
-const popupWithProfileForm = new PopupWithForm(selectors.formProfileSelector, formProfileSubmitHandler);
-
+const popupWithProfileForm = new PopupWithForm(
+  selectors.formProfileSelector,
+  formProfileSubmitHandler
+);
+const userInfoInstance = new UserInfo({
+  userNameSelector: selectors.profileNameElement,
+  userJobSelector: selectors.profileJobElement,
+});
 
 profileEditButton.addEventListener("click", () => {
   popupWithProfileForm.open();
-  nameInput.value = profileNameElement.textContent;
-  jobInput.value = profileJobElement.textContent;
-  
+  const userInfo = userInfoInstance.getUserInfo();
+  nameInput.value = userInfo.username;
+  jobInput.value = userInfo.job;
+  userInfoInstance.setUserInfo(userInfo);
   formValidators.profileForm.resetValidation();
-  
 });
 
 function formProfileSubmitHandler(evt) {
   evt.preventDefault();
-  profileNameElement.textContent = nameInput.value;
-  profileJobElement.textContent = jobInput.value;
+  userInfoInstance.setUserInfo({
+    username: nameInput.value,
+    job: jobInput.value,
+  });
   evt.target.reset();
   popupWithProfileForm.close();
 }
